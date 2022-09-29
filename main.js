b22390d9c8a00bf4220d0410140b080e2adb6014
Juliana Mota Carneiro <jmcarneiro@gmail.com>
2022-09-29 16:23:52 -0300

    Inclusão do som nas teclas e ajuste no layout

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 // document.querySelector('input[type=tel]');
 
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento && elemento.localName === 'audio') {
+    elemento.play();
+  } else {
+    alert('Elemento não encontrado ou seletor inválido');
+  }
+}
+
+
 const listaDeTeclas = document.querySelectorAll('input[type=button]');
 console.log(listaDeTeclas);
 
@@ -8,11 +19,14 @@ console.log(inputTel);
 
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
+    const idAudio = `#som_tecla_dial`;
     console.log(contador);
 
     tecla.onclick = function () {
-        inputTel.value = inputTel.value + tecla.value
+        tocaSom(idAudio);
+        inputTel.value = inputTel.value + tecla.value;
     }
+    
 
     tecla.onkeydown = function (evento) {
         if(evento.code === "Enter" || evento.code === "Space") {
